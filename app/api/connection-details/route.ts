de60ab536c4accd7c7b437bd9754fc8eeecf3f2d
kofsitho87 <kofsitho@gmail.com>
2025-07-04 16:59:08 +0900

    upt

---
+++ app/api/connection-details/route.ts
@@ -0,0 +1,82 @@
+import { NextResponse } from 'next/server';
+import { AccessToken, type AccessTokenOptions, type VideoGrant, RoomServiceClient } from 'livekit-server-sdk';
+
+// NOTE: you are expected to define the following environment variables in `.env.local`:
+const API_KEY = process.env.LIVEKIT_API_KEY;
+const API_SECRET = process.env.LIVEKIT_API_SECRET;
+const LIVEKIT_URL = process.env.LIVEKIT_URL;
+
+// don't cache the results
+export const revalidate = 0;
+
+export type ConnectionDetails = {
+  serverUrl: string;
+  roomName: string;
+  participantName: string;
+  participantToken: string;
+};
+
+export async function GET() {
+  try {
+    if (LIVEKIT_URL === undefined) {
+      throw new Error('LIVEKIT_URL is not defined');
+    }
+    if (API_KEY === undefined) {
+      throw new Error('LIVEKIT_API_KEY is not defined');
+    }
+    if (API_SECRET === undefined) {
+      throw new Error('LIVEKIT_API_SECRET is not defined');
+    }
+
+    // Generate participant token
+    const clientId = 507;
+    const clientName = '와이즈병원';
+    const clientPhone = '010-1234-5678';
+    const clientAddress = '서울시 강남구 역삼동';
+    const siteId = 'wisedental';
+
+    // const participantName = `${clientId}_${clientName}_${clientPhone}_${clientAddress}_${siteId}`;
+    // const participantIdentity = `voice_assistant_user_${Math.floor(Math.random() * 10_000)}`;
+    const participantName = "Dan";
+    const participantIdentity = `53e66d3a-b767-45d5-9ff1-3735572d4486`;
+    const roomName = `voice_assistant_room_${Math.floor(Math.random() * 10_000)}`;
+    const participantToken = await createParticipantToken(
+      { identity: participantIdentity, name: participantName },
+      roomName
+    );
+    
+
+    // Return connection details
+    const data: ConnectionDetails = {
+      serverUrl: LIVEKIT_URL,
+      roomName,
+      participantToken: participantToken,
+      participantName,
+    };
+    const headers = new Headers({
+      'Cache-Control': 'no-store',
+    });
+    return NextResponse.json(data, { headers });
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error);
+      return new NextResponse(error.message, { status: 500 });
+    }
+  }
+}
+
+function createParticipantToken(userInfo: AccessTokenOptions, roomName: string) {
+  const at = new AccessToken(API_KEY, API_SECRET, {
+    ...userInfo,
+    ttl: '15m',
+  });
+  const grant: VideoGrant = {
+    room: roomName,
+    roomJoin: true,
+    canPublish: true,
+    canPublishData: true,
+    canSubscribe: true,
+  };
+  at.addGrant(grant);
+  return at.toJwt();
+}
